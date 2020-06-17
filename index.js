7906958c35a317f7dcb9563a989f332b4f54cf3a
Jakub Sukiennik <kuba1207710@gmail.com>
2020-06-17 17:47:37 +0200

    socket server

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express();
+const express = require('express');
+const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const dotenv = require('dotenv')
 const connectDB = require('./utils/connectDatabase')
 const passport = require('passport');
 dotenv.config({ path: "./.env" });
 require('./config/passport')(passport);
+
 // bodyparser
 app.use(express.json())
-
 // connecting to database
 connectDB();
 app.use(passport.initialize());
@@ -16,6 +18,6 @@ app.use('/auth', require('./routes/AuthRoutes'))
 
 // listening
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`App working on port ${PORT}`)
 })
