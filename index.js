466f6eea6b170bda71481f88fc9152f04e30edfa
Jakub Sukiennik <kuba1207710@gmail.com>
2020-06-18 08:28:43 +0200

    adding chatroom model and basic socket

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const express = require('express');
-const app = express()
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const dotenv = require('dotenv')
-const connectDB = require('./utils/connectDatabase')
-const passport = require('passport');
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const socketioJWT = require("socketio-jwt");
+const dotenv = require("dotenv");
+const connectDB = require("./utils/connectDatabase");
+const passport = require("passport");
+const User = require("./models/User");
+const ChatRoom = require('./models/ChatRoom')
 dotenv.config({ path: "./.env" });
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // bodyparser
-app.use(express.json())
+app.use(express.json());
 // connecting to database
 connectDB();
 app.use(passport.initialize());
-// auth routes 
-app.use('/auth', require('./routes/AuthRoutes'))
+// auth routes
+app.use("/auth", require("./routes/AuthRoutes"));
+
+io.use(
+  socketioJWT.authorize({
+    secret: process.env.secret,
+    handshake: true
+  })
+);
+io.on("connection", socket => {
+  User.findById(socket.decoded_token.sub).then(user => console.log(user.nick));
+
+  socket.on('join', ({name, room}, callback) => {
+    console.log(name,room)
+  })
+  socket.on('disconnect', () => {
+      console.log('User had left')
+  })
+});
 
 // listening
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`App working on port ${PORT}`)
-})
+  console.log(`App working on port ${PORT}`);
+});
