5dffa15f4ab58241bb9f8dc10b315eb97bbe309a
Jakub Sukiennik <kuba1207710@gmail.com>
2020-09-19 16:49:56 +0200

    fixing errors

--- models/ChatRoom.js
+++ models/ChatRoom.js
@@ -3,33 +3,30 @@ const moment = require("moment");
 const ChatroomSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
   },
   founder: {
     type: String,
-    required: true
   },
   users: {
     type: [String],
-    required: true
   },
   messages: [
     {
       content: {
-        type: String
+        type: String,
       },
       issuedBy: {
-        type: String
+        type: String,
       },
       emailOfIssuer: {
-        type: String
+        type: String,
       },
       date: {
         type: String,
-        default: moment().format("MMMM Do YYYY, h:mm:ss a")
-      }
-    }
-  ]
+        default: moment().format("MMMM Do YYYY, h:mm:ss a"),
+      },
+    },
+  ],
 });
 
 module.exports = mongoose.model("ChatRoom", ChatroomSchema);
